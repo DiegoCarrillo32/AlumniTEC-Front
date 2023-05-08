35a9116e36c3818222a6fb06c8e85c15a9b85c23
David Gonzalez <davidega12@gmail.com>
2023-05-07 23:48:17 -0600

    Add Study

--- src/pages/DetailedAlumni/DetailedAlumni.jsx
+++ src/pages/DetailedAlumni/DetailedAlumni.jsx
@@ -5,11 +5,46 @@ import { useParams } from 'react-router-dom'
 import { fetchAlumniById } from '../../api/fetch_alumni'
 import { Item } from '../../components/Item/Item'
 import PostStudy from '../../components/PostStudy/PostStudy'
+import { Box, Modal } from '@mui/material'
+import { Input } from '../../components/Input/Input'
+import { Button } from '../../components/Button/Button'
+import { create_postStudy } from '../../api/create_postStudy'
+import { toast } from 'sonner'
 
 export const DetailedAlumni = () => {
   const {id} = useParams()
   const [Alumni, setAlumni] = useState({})
   const [Loading, setLoading] = useState(true)
+
+  const [open, setOpen] = React.useState(false)
+  const handleClose = () => setOpen(false)
+  const handleOpen = () => setOpen(true)
+
+
+
+const handleSubmitCreate = async (e) => {
+   e.preventDefault ()
+   const data = {
+    name:e.target.name.value,
+    institution:e.target.institution.value,
+    type:e.target.type.value,
+    initDate:e.target.init.value,
+    endDate:e.target.end.value,
+    alumniId:id
+   };
+
+   const resp = create_postStudy(data);
+
+   if(resp != null){
+    toast.success("Creado con éxito.")
+    handleClose()
+   }else{
+    toast.error("Ha ocurrido un error");
+   }
+
+}
+
+
   useEffect(()=>{
     (async ()=> {
       setLoading(true)
@@ -82,6 +117,26 @@ export const DetailedAlumni = () => {
       <div>
       <h2 className='text-2xl md:text-4xl md:mb-2'>Estudios Posteriores</h2>
       <span className='text-gray-400 text-xl'>Informacion de estudios posteriores</span>
+      
+      <Button onClick={ handleOpen }> Crear Estudio </Button>
+
+          <Modal
+            open={open}
+            onClose={handleClose}
+            >
+            <Box className="absolute top-1/4 left-1/4 bg-slate-50 h-fit p-4  rounded-md ">
+                <form onSubmit={handleSubmitCreate}>
+                    {/* <Input type='textarea' placeholder={"Description"} className='m-4' name='description'/> */}
+                    <Input  placeholder={"Nombre"} className='m-4' name='name'/>
+                    <Input  placeholder={"Institucion"} className='m-4' name='institution' />
+                    <Input  placeholder={"Tipo"} className='m-4' name='type' />
+                    <Input type='date' placeholder={"Fecha de inicio"} className='m-4' name='init'  required={true}/>
+                    <Input type='date' placeholder={"Fecha de cierre"} className='m-4' name='end'  required={true}/>
+                    <Button className="m-4">Añadir estudio</Button>
+                </form>
+            </Box>
+            </Modal>
+
     </div>
 
     <section className='flex flex-col overflow-y-scroll'>
